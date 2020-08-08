4bb5908dae68ea043237240e3465bb98000c0175
Areej <rejmunti@gmail.com>
2020-08-08 03:54:54 +0300

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'dev-jvxw-fhd.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'cafe'
+API_AUDIENCE = 'coffee'
 
 # AuthError Exception
 '''
